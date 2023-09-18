d4419dada75542ee40d753dda0ca1fa931600859
user <user@ip-192-168-1-4.us-west-2.compute.internal>
2023-09-18 18:59:26 -0500

    ejercicio2.11 -2.14

--- src/App.js
+++ src/App.js
@@ -1,78 +1,95 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Filtro from './components/Filter'
-import FormularioAgregarPersona from './components/FormularioAgregarPersona'
-import ListaPersonas from './components/ListaPersonas'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-  const [searchName, setSearchName] = useState('')
+  const [countries, setCountries] = useState([])
+  const [searchName, setSearchName] = useState('Colombia')
+  const [message, setMessage] = useState('');
+  const [showDetails, setShowDetails] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-  }, [])
+    if (searchName.trim() !== '') {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${searchName}`)
+        .then((response) => {
+          console.log('promise fulfilled');
+          const data = response.data;
 
-  const addName = (event) => {
-    event.preventDefault()
-
-    const nombreExistente = persons.find((person) => person.name === newName);
-
-    if (nombreExistente) {
-      window.alert(`${newName} ya existe en la lista de telefonos`);
-    } else {
-      const personObject = {
-        name: newName,
-        phone: newPhone,
-        id: persons.length + 1,
-      }
-
-      setPersons(persons.concat(personObject))
+          if (data.length > 10) {
+            setMessage('Demasiados resultados. Por favor, sé más específico en tu consulta.');
+            setCountries([]);
+            setShowDetails(false);
+          } else if (data.length === 0) {
+            setMessage('No se encontraron resultados.');
+            setCountries([]);
+            setShowDetails(false);
+          } else {
+            setMessage('');
+            setCountries(data);
+            setShowDetails(false);
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
     }
-
-    setNewName('')
-    setNewPhone('')
-  }
-
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handlePhoneChange = (event) => {
-    setNewPhone(event.target.value)
-  }
+    else {
+      setMessage('');
+      setCountries([]);
+      setShowDetails(false);
+    }
+  }, [searchName])
 
   const handleSearchName = (event) => {
     setSearchName(event.target.value)
   }
 
-  const personsToShow = searchName === ''
-  ? persons
-  : persons.filter((person) => person.name.toLowerCase() === searchName.toLowerCase()); 
+  const handleShowDetails = (country) => {
+    setSelectedCountry(country);
+    setShowDetails(true);
+  };
 
   return (
     <div>
-       <Filtro value={searchName} onChange={handleSearchName} />
-      <h2>Phonebook</h2>
-      <FormularioAgregarPersona
-        onSubmit={addName}
-        name={newName}
-        phone={newPhone}
-        onNameChange={handleNameChange}
-        onPhoneChange={handlePhoneChange}
-      />
-      <h2>Numbers</h2>
-      <ListaPersonas persons={personsToShow} />
+      <form>
+        <div>
+          Buscar País: <input value={searchName} onChange={handleSearchName} />
+        </div>
+      </form>
+      <div>
+        <h2>Países</h2>
+        {message && <p>{message}</p>}
+        {countries.length > 1 && (
+          <ul>
+            {countries.map((country) => (
+              <li key={country.id}>
+                {country.name.common}{' '}
+                <button onClick={() => handleShowDetails(country)}>Mostrar Detalles</button>
+              </li>
+            ))}
+          </ul>
+        )}
+        {countries.length === 1 || showDetails ? (
+          <div>
+            <h3>{selectedCountry?.name?.common || countries[0].name?.common }</h3>
+            <p>Capital: {selectedCountry?.capital || countries[0].capital}</p>
+            <p>Población: {selectedCountry?.population || countries[0].population}</p>
+            <p>Área: {selectedCountry?.area ||  countries[0].area} km²</p>
+            <p>Idiomas:</p>
+            <ul>
+              {(selectedCountry?.languages)&&
+                Object.values((selectedCountry.languages)).map((language, index) => (
+                  <li key={index}>{language}</li>
+                ))}
+            </ul>
+            <img src={selectedCountry?.flags?.png || countries[0].flags?.png } alt={`Bandera de ${selectedCountry?.name?.common || countries[0].name?.common}`} />
+          </div>
+        ) : null}
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default App
